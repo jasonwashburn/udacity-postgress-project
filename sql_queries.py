23e1cfed0bd3cbf856a693cf8ac50a97c1c6795d
Jason Washburn <wburn@wburn.net>
2020-07-12 10:54:11 -0500

    Adds 'create' and 'drop' table statements
    Adds create and drop table statements for songplays, users, songs,
    artists, and time

--- sql_queries.py
+++ sql_queries.py
@@ -1,25 +1,32 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time int, user_id int, level text, song_id text, artist_id text, session_id int, location text, user_agent text)")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id int,
+                        start_time bigint, user_id int, level text, song_id text,
+                        artist_id text, session_id int, location text,
+                        user_agent text)""")
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int,
+                        first_name text, last_name text, gender char, level text)
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id text, title text,
+                        artist_id text, year int, duration float)
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id text,
+                        name text, location text, latitude float, longitude float)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time bigint,
+                        hour int, day int, month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
