bbc7674f8c8a84a02760b4249974f74c6bd72745
Jason Washburn <wburn@wburn.net>
2020-07-14 18:14:42 -0500

    Change user insert statement to update on conflict

--- sql_queries.py
+++ sql_queries.py
@@ -39,7 +39,8 @@ songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level,
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name,
                         gender, level)
-                        VALUES(%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING
+                        VALUES(%s, %s, %s, %s, %s) ON CONFLICT (user_id)
+                        DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year,
